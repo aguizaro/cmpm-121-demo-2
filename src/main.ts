ba7ea74a01517bc09b4cd1447aad8fdf49998c8e
Antonio Guizar Orozco <aguizaro@ucsc.edu>
2023-10-14 15:18:31 -0700

    Step 1 Completed

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
+const canvasWidth = 256;
+const canvasHeight = 256;
 
-const gameName = "Tony's Game";
-
+const gameName = "StickerPAD";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
-app.append(header);
+
+const canvas = document.createElement("canvas");
+canvas.id = "myCanvas";
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
+const ctx = canvas.getContext("2d")!;
+
+ctx.fillStyle = "beige";
+ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+app.append(header, canvas);
