cf6b37c920813308a434289c316ef85da78f0d65
Antonio Guizar Orozco <aguizaro@ucsc.edu>
2023-10-17 23:27:41 -0700

    Step 5 completed

--- src/main.ts
+++ src/main.ts
@@ -1,55 +1,46 @@
 import "./style.css";
 
-//------------------------------------------------------------------------------------
+//----------------------------class def + helper functions-------------------------------------------------------
+// class def for a line object
+class LineCommand {
+  points: { x: number; y: number }[];
+  length: number;
 
-function notifyChange() {
-  const drawingChangedEvent = new Event("drawing-changed");
-  canvas.dispatchEvent(drawingChangedEvent);
-}
-// Draw line segments if pen is down
-function addPoint(e: MouseEvent) {
-  // pen style
-  ctx.lineWidth = 1.5;
-  ctx.lineCap = "round";
-  ctx.strokeStyle = "black";
-  //add points to current stroke
-  currentStroke.push({
-    x: e.offsetX,
-    y: e.offsetY,
-  });
-  notifyChange();
-}
+  constructor(x?: number, y?: number) {
+    this.points = x && y ? [{ x, y }] : [];
+    this.length = x && y ? 1 : 0;
+  }
+  // copy constructor
+  copyFrom(original: LineCommand) {
+    this.points = [...original.points];
+    this.length = original.length;
+  }
 
-// clear canvas and redraw all line segments
-function redraw() {
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  penStrokes.forEach((stroke) => {
-    if (stroke.length > 1) {
-      ctx.beginPath();
-      stroke.forEach((point) => {
-        ctx.lineTo(point.x, point.y);
-        ctx.stroke();
-      });
+  display(ctx: CanvasRenderingContext2D) {
+    ctx.lineWidth = lineWidth;
+    ctx.lineCap = "round";
+    ctx.strokeStyle = "black";
+    const { x, y } = this.points[0];
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    for (const { x, y } of this.points) {
+      ctx.lineTo(x, y);
     }
-  });
+    ctx.stroke();
+  }
+
+  extend(x: number, y: number) {
+    this.points.push({ x, y });
+    this.length++;
+  }
 }
 
-//
-
-// undo last stroke and add it to redoStack
-function undo() {
-  if (penStrokes.length < 1) return;
-  redoStack.push(penStrokes.pop()!);
-  notifyChange();
-}
-// redo last stroke and add pop it from redoStack
-function redo() {
-  if (redoStack.length < 1) return;
-  penStrokes.push(redoStack.pop()!);
-  notifyChange();
+// notify observer of change by dispatching a new event
+function notifyChange() {
+  canvas.dispatchEvent(new Event("drawing-changed"));
 }
 
-//-------------------------------------------------------------------------------------
+//--------------------------------create HTML elements + canvas + global vars ------------------------------------------------
 // set up main div
 const app: HTMLDivElement = document.querySelector("#app")!;
 // title
@@ -57,10 +48,10 @@ const gameName = "StickerPAD";
 document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
-
 // create canvas
 const canvasWidth = 256;
 const canvasHeight = 256;
+const lineWidth = 1.5;
 const canvas = document.createElement("canvas");
 canvas.id = "myCanvas";
 canvas.width = canvasWidth;
@@ -69,69 +60,76 @@ const ctx = canvas.getContext("2d")!;
 ctx.fillStyle = "beige";
 ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
+//set up pen tools
+let penDown = false;
+let commands: LineCommand[] = [];
+let redoStack: LineCommand[] = [];
+
+//---------------------------------create buttons---------------------------------------------------------
 //clear canvas button
 const clearButton: HTMLButtonElement = document.createElement("button");
 clearButton.innerText = "Clear";
 clearButton.classList.add("button-container");
 //clear canvas when button is clicked
 clearButton.addEventListener("click", () => {
-  penStrokes.length = 0; //clear stroke history
-  currentStroke = [];
-  redoStack.length = 0;
+  commands = []; //clear stroke history
+  redoStack = [];
   notifyChange();
-  //penStrokes.push(currentStroke); //add reference to current stroke - current is updated with points when user draws (*important*)
 });
 // undo button
 const undoButton: HTMLButtonElement = document.createElement("button");
 undoButton.innerText = "Undo";
 undoButton.classList.add("button-container");
 undoButton.addEventListener("click", () => {
-  undo();
+  if (!commands.length) return;
+  redoStack.push(commands.pop()!);
+  notifyChange();
 });
 // redo button
 const redoButton: HTMLButtonElement = document.createElement("button");
 redoButton.innerText = "Redo";
 redoButton.classList.add("button-container");
 redoButton.addEventListener("click", () => {
-  redo();
-});
-
-//------------------------------------------------------------------------------------
-//set up pen tools
-let penDown = false;
-const penStrokes: { x: number; y: number }[][] = [];
-let currentStroke: { x: number; y: number }[] = [];
-const redoStack: { x: number; y: number }[][] = [];
-//penStrokes.push(currentStroke); //add reference to current stroke to see live updates when re-drawing
-
-//Set up listners for mouse to draw
-canvas.addEventListener("mousedown", (e) => {
-  penDown = true;
-  currentStroke = [];
-  penStrokes.push(currentStroke);
-  redoStack.splice(0, redoStack.length);
-  currentStroke.push({ x: e.offsetX, y: e.offsetY });
+  if (!redoStack.length) return;
+  commands.push(redoStack.pop()!);
   notifyChange();
 });
-canvas.addEventListener("mousemove", (e) => {
-  if (penDown) addPoint(e);
-});
-canvas.addEventListener("mouseup", () => {
-  penDown = false;
-  currentStroke = []; // clear current stroke points
-});
 
-// avoids weird line segments when pen is down, exits canvas, and then enters canvas in new location
+//---------------------------------event listeners--------------------------------------------
+canvas.addEventListener("mousedown", (e) => {
+  console.log("down");
+  penDown = true;
+  //start new line with fist point
+  commands.push(new LineCommand(e.offsetX, e.offsetY));
+});
+canvas.addEventListener("mousemove", (e) => {
+  if (penDown) {
+    console.log("draw");
+    commands[commands.length - 1].extend(e.offsetX, e.offsetY);
+    commands[commands.length - 1].display(ctx);
+    redoStack = [];
+  } else {
+    console.log("move");
+  }
+});
 canvas.addEventListener("mouseleave", () => {
-  if (currentStroke.length > 0) penStrokes.push(currentStroke.slice()); // add current stroke to strokes array
-  currentStroke = [];
+  penDown = false;
+  console.log("leave");
+});
+document.addEventListener("mouseup", () => {
+  penDown = false;
+  //clear currentLine
+  console.log("up");
 });
 // observer for drawing-changed event - clears and redraws canvas
 canvas.addEventListener("drawing-changed", () => {
-  redraw();
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  commands.forEach((line) => {
+    if (line.length) {
+      line.display(ctx);
+    }
+  });
 });
-//------------------------------------------------------------------------------------
 
-//Todo: make it so players can click to make a dot (mousedown -> mouseup = dot)
-
+//-------------------------add HTML Elements to main app div------------------------------------------
 app.append(header, canvas, clearButton, undoButton, redoButton);
