8fef63d5c2879e904dcd60f1eee3b1faa0601237
Antonio Guizar Orozco <aguizaro@ucsc.edu>
2023-10-18 14:54:30 -0700

    step 7 completed

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import "./style.css";
 
 //----------------------------class def + helper functions-------------------------------------------------------
-// class def for a line object
 class LineCommand {
   points: { x: number; y: number }[];
   length: number;
@@ -26,16 +25,49 @@ class LineCommand {
     }
     ctx.stroke();
   }
-
   extend(x: number, y: number) {
     this.points.push({ x, y });
     this.length++;
   }
 }
+class CursorComand {
+  x: number;
+  y: number;
+  size: number;
+  color: string;
+  constructor(x: number, y: number, size: number, color: string) {
+    this.x = x;
+    this.y = y;
+    this.size = size * 4;
+    this.color = color;
+  }
+  display(ctx: CanvasRenderingContext2D) {
+    const originalFillStyle = ctx.fillStyle;
+    ctx.fillStyle = this.color;
+    ctx.font = `${Math.max(7, this.size)}px monospace`;
+    if (this.size <= 4) {
+      ctx.fillText("+", this.x - 2, this.y + 3);
+    } else {
+      ctx.fillText("+", this.x - 8, this.y + 3);
+    }
+    ctx.fillStyle = originalFillStyle;
+  }
+}
 
 // notify observer of change by dispatching a new event
-function notifyChange() {
-  canvas.dispatchEvent(new Event("drawing-changed"));
+function notifyChange(name: string) {
+  canvas.dispatchEvent(new Event(name));
+}
+
+//redrar
+function redraw() {
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  commands.forEach((line) => {
+    if (line.length) {
+      line.display(ctx);
+    }
+  });
+  if (cursorComand) cursorComand.display(ctx);
 }
 
 //--------------------------------create HTML elements + canvas + global vars ------------------------------------------------
@@ -53,7 +85,7 @@ const canvas = document.createElement("canvas");
 canvas.id = "myCanvas";
 canvas.width = canvasWidth;
 canvas.height = canvasHeight;
-canvas.style.cursor = "crosshair";
+canvas.style.cursor = "none";
 const ctx = canvas.getContext("2d")!;
 ctx.fillStyle = "beige";
 ctx.fillRect(0, 0, canvasWidth, canvasHeight);
@@ -78,6 +110,7 @@ let redoStack: LineCommand[] = [];
 let lineWidth = 1;
 let penColor = "black";
 const colors = ["black", "red", "blue", "green", "orange", "white", "yellow"];
+let cursorComand: CursorComand | null = null;
 
 //---------------------------------create buttons---------------------------------------------------------
 //clear canvas button
@@ -87,7 +120,7 @@ clearButton.innerText = "Clear";
 clearButton.addEventListener("click", () => {
   commands = []; //clear stroke history
   redoStack = [];
-  notifyChange();
+  notifyChange("drawing-changed");
 });
 buttons.appendChild(clearButton);
 // undo button
@@ -96,7 +129,7 @@ undoButton.innerText = "Undo";
 undoButton.addEventListener("click", () => {
   if (!commands.length) return;
   redoStack.push(commands.pop()!);
-  notifyChange();
+  notifyChange("drawing-changed");
 });
 buttons.appendChild(undoButton);
 // redo button
@@ -105,7 +138,7 @@ redoButton.innerText = "Redo";
 redoButton.addEventListener("click", () => {
   if (!redoStack.length) return;
   commands.push(redoStack.pop()!);
-  notifyChange();
+  notifyChange("drawing-changed");
 });
 buttons.appendChild(redoButton);
 // line width button
@@ -115,7 +148,10 @@ lineWidthButton.addEventListener("click", () => {
   lineWidth = lineWidth < 10 ? lineWidth + 1 : 1;
   ctx.lineWidth = lineWidth;
   lineWidthButton.innerText = `${lineWidth}px`;
+  if (cursorComand) cursorComand.size = lineWidth;
+  notifyChange("tool-moved");
 });
+//button to change pen color
 const colorButton: HTMLButtonElement = document.createElement("button");
 colorButton.innerText = `${penColor}`;
 colorButton.addEventListener("click", () => {
@@ -133,7 +169,7 @@ colorButton.addEventListener("click", () => {
   }
   colorButton.style.backgroundColor = penColor;
 });
-
+// add buttons for pen marker tools
 markerTools.append(lineWidthButton, colorButton);
 
 //---------------------------------event listeners--------------------------------------------
@@ -142,34 +178,39 @@ canvas.addEventListener("mousedown", (e) => {
   penDown = true;
   //start new line with fist point
   commands.push(new LineCommand(e.offsetX, e.offsetY, lineWidth, penColor));
+  redoStack = [];
 });
 canvas.addEventListener("mousemove", (e) => {
   if (penDown) {
+    cursorComand = null;
     console.log("draw");
     commands[commands.length - 1].extend(e.offsetX, e.offsetY);
     commands[commands.length - 1].display(ctx);
-    redoStack = [];
-  } else {
-    console.log("move");
+    cursorComand = new CursorComand(e.offsetX, e.offsetY, lineWidth, penColor);
   }
+  cursorComand = new CursorComand(e.offsetX, e.offsetY, lineWidth, penColor);
+  notifyChange("tool-moved");
 });
-canvas.addEventListener("mouseleave", () => {
+canvas.addEventListener("mouseout", () => {
   penDown = false;
   console.log("leave");
+});
+canvas.addEventListener("mouseenter", (e) => {
+  cursorComand = new CursorComand(e.offsetX, e.offsetY, lineWidth, penColor);
+  notifyChange("tool-moved");
 });
 document.addEventListener("mouseup", () => {
   penDown = false;
   //clear currentLine
   console.log("up");
 });
+// observer for tool-moved event
+canvas.addEventListener("tool-moved", () => {
+  redraw();
+});
 // observer for drawing-changed event - clears and redraws canvas
 canvas.addEventListener("drawing-changed", () => {
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  commands.forEach((line) => {
-    if (line.length) {
-      line.display(ctx);
-    }
-  });
+  redraw();
 });
 
 //-------------------------add HTML Elements to main app div------------------------------------------
